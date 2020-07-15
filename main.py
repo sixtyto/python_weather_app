d4ec600e185668d30c5e7c91fcd0f65ae6cfbb4e
Piotr Czajkowski <sixtyto@gmail.com>
2020-07-15 22:19:44 +0200

    add main function

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-def main():
+from requests import get
+from json import loads
+
+
+def main(city):
+    find = False
     url = 'https://danepubliczne.imgw.pl/api/data/synop'
+    response = get(url)
+    for data in loads(response.text):
+        if data['stacja'] == city:
+            print(data)
+            find = True
+    if not find:
+        print("city not found")
 
 
 if __name__ == '__main__':
     print("Weather app")
-    main()
+    city = input('Type city name to check: ')
+    main(city.capitalize())
